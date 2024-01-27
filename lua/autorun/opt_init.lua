f6a7f6e2681bd250d9403ca14a588c5e010bc7eb
shockpast <shockpast@gmail.com>
2024-01-27 23:26:18 +0300

    fix: moved "sh_" to "opt_init.lua"

--- lua/autorun/opt_init.lua
+++ lua/autorun/opt_init.lua
@@ -1,11 +1,16 @@
+SOptimization = SOptimization or {} -- shockpast. Optimization. (awful naming)
+
+local color_cornflower = Color(84, 109, 229)
+local color_gray = Color(210, 218, 226)
+
+function SOptimization.Log(...)
+  MsgC(color_cornflower, "[#] ", color_gray, ..., "\n")
+end
+
 if SERVER then
   AddCSLuaFile("optimization/cl_optimization.lua")
-  AddCSLuaFile("optimization/sh_optimization.lua")
-
-  include("optimization/sh_optimization.lua")
 end
 
 if CLIENT then
   include("optimization/cl_optimization.lua")
-  include("optimization/sh_optimization.lua")
 end
--- lua/optimization/sh_optimization.lua
+++
@@ -1,8 +0,0 @@
-SOptimization = SOptimization or {} -- shockpast. Optimization. (awful naming)
-
-local color_cornflower = Color(84, 109, 229)
-local color_gray = Color(210, 218, 226)
-
-function SOptimization.Log(...)
-  MsgC(color_cornflower, "[#] ", color_gray, ..., "\n")
-end
